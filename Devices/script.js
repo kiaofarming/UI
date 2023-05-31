b1edd2b255057054450b0bfc56fdf377b9c38bbc
HsiangOOR <126255937+HsiangOOR@users.noreply.github.com>
2023-05-31 20:53:25 +0800

    Add files via upload

--- Devices/script.js
+++ Devices/script.js
@@ -1,21 +1,24 @@
 const API_HOST = "https://mqtt.kiaofarming.com";
 const PROXY_HOST = 'https://cors-anywhere.herokuapp.com/';
 const API_VER = "v1";
-
-
-
+/*
 var deviceList = [{
   name: '調光LED燈',
   tags: ['風扇', '灑水'],
   ssid: 'wf8011',
   devID: 'wjWXd'
-}, {
+},{
   name: 'Sean專用機',
-  tags: ['開關', '通知燈'],
+  tags: ['關關', '通知燈'],
   ssid: 'wf8010',
   devID: 'QrkNV'
-}]
+}];
+*/
 
+let url = '?openExternalBrowser=1';
+window.history.pushState(null, null, url);
+
+var deviceList = new Array();
 
 function set_switch(dev_id, port, sw, cb) {
   let req_data = {};
@@ -72,9 +75,13 @@ function process_switch_response(resp) {
   }
 }
 
-
 function viewDevice(key, devices, ports) {
   let portNum = ports;
+
+  const zoneId = document.getElementById(`zone${key}`);
+  if (zoneId != null) {
+    return;
+  }
 
   const device = devices[key];
 
@@ -142,57 +149,124 @@ function viewDevice(key, devices, ports) {
   });
 }
 
-function state_refresh(dev) {
-  for (let j = 0; j < deviceList.length; j++) {
-    if (dev.ssid == deviceList[j]['ssid'] && dev.device == deviceList[j]['devID']) {
-      const zoneState = document.querySelectorAll(`#zone${j} span`);
-      const zoneButton = document.querySelectorAll(`#zone${j} div`);
-      const devName = document.querySelectorAll(`#devList h3`);
+async function state_refresh(dev, key) {
 
-      if (dev.online != undefined) {
-        if (dev.online == true) {
-          devName[j].style.backgroundColor = "#1C8686";
-        } else {
-          devName[j].style.backgroundColor = "#a0a0a0";
-        }
-      }
+  await viewDevice(key, deviceList, 2);
 
-      if (dev.switch != undefined) {
-        for (let i = 0; i < dev.switch.length; i++) {
-          let st = 'OFF';
-          let color = '#E0E0E0';
-          if (dev.switch[i] == true) {
-            st = 'ON';
-            color = '#8FCDE4';
-          }
-          zoneState[i].textContent = st;
-          zoneButton[i].style.backgroundColor = color;
-        }
+  const zoneState = document.querySelectorAll(`#zone${key} span`);
+  const zoneButton = document.querySelectorAll(`#zone${key} div`);
+  const devName = document.querySelectorAll(`#devList h3`);
+
+  if (dev.online != undefined) {
+    if (dev.online == true) {
+      devName[key].style.backgroundColor = "#1C8686";
+    } else {
+      devName[key].style.backgroundColor = "#a0a0a0";
+    }
+  }
+
+  if (dev.switch != undefined) {
+    for (let i = 0; i < dev.switch.length; i++) {
+      let st = 'OFF';
+      let color = '#E0E0E0';
+      if (dev.switch[i] == true) {
+        st = 'ON';
+        color = '#8FCDE4';
       }
-      return;
+      zoneState[i].textContent = st;
+      zoneButton[i].style.backgroundColor = color;
     }
   }
 }
 
+function btnSubmit() {
+
+  var newDevice = {
+    name: document.getElementById('name').value,
+    tags: [document.getElementById('tags1').value, document.getElementById('tags2').value],
+    ssid: document.getElementById('valid_ssid').value,
+    devID: document.getElementById('valid_dev_id').value
+  };
+
+  if (newDevice.name === "" || newDevice.ssid === "" || newDevice.devID === "") {
+    return;
+  }
+
+  console.log(newDevice);
+  deviceList.push(newDevice);
+
+  var strJSON = JSON.stringify(deviceList);
+  localStorage.setItem('myList', strJSON);
+}
+
+function showDeviceMenu() {
+  const devMenu = document.getElementById('devMenu');
+  var devItem = null;
+
+  for (let i = 0; i < deviceList.length; i++) {
+    // 創建元素
+    const buttonContainer = document.createElement('div');
+    const devItem = document.createElement('li');
+    const deleteItem = document.createElement('div');
+
+    // 設置元素的屬性和內容
+    buttonContainer.setAttribute('class', 'button-container');
+    devItem.setAttribute('class', 'useDevice');
+    devItem.innerHTML = deviceList[i].name;
+    deleteItem.setAttribute('class', 'delete');
+    deleteItem.textContent = '—';
+
+    // 將元素添加到容器中
+    buttonContainer.appendChild(devItem);
+    buttonContainer.appendChild(deleteItem);
+
+    // 將容器添加到文檔中的某個元素中
+    const parentElement = document.querySelector('#devMenu'); // 替換為實際的父元素選擇器
+    parentElement.appendChild(buttonContainer);
+
+    deleteItem.addEventListener('click', () => {
+      buttonContainer.remove(); // 移除包含 deleteItem 和 devItem 的 buttonContainer
+    });
+
+  }
+  
+}
+
+
+
+
 window.onload = () => {
+  var strJSON = localStorage.getItem('myList');
 
-
-  viewDevice(0, deviceList, 2);
-  viewDevice(1, deviceList, 2);
+  if (strJSON != null && strJSON != "") {
+    console.log(strJSON);
+    deviceList = JSON.parse(strJSON);
+    showDeviceMenu();
+  }
 
   //*跳出視窗*//
   let btn = document.querySelector("#show");
   let infoModal = document.querySelector("#infoModal");
+  var infoItems = document.querySelectorAll("#infoModal input");
+
   btn.addEventListener("click", function () {
-    console.log("Show button clicked"); 
+    console.log("Show button clicked");
+    for (let i = 0; i < infoItems.length; i++) {
+      infoItems[i].setAttribute("required", "required");
+    }
     infoModal.showModal();
   });
 
   let close = document.querySelector("#close");
   close.addEventListener("click", function () {
+    for (let i = 0; i < infoItems.length; i++) {
+      infoItems[i].removeAttribute("required");
+    }
     infoModal.close();
   });
-  //**視窗 */
+
+  if (deviceList.length <= 0)
+    return;
 
   //  const urlAPI = `${API_HOST}/${API_VER}/notify?device=${deviceList[0].ssid},${deviceList[0].devID};${deviceList[1].ssid},${deviceList[1].devID}`;
   let urlAPI = `${API_HOST}/${API_VER}/notify?device=`;
@@ -209,20 +283,34 @@ window.onload = () => {
   // Server-Send Event example
   evt_src.addEventListener("init", (ev) => { // return all device data array
     devs = JSON.parse(ev.data);
-    devs.forEach((e, index) => {
-      let dev = e;
-      state_refresh(dev);
-    });
+    for (let j = 0; j < deviceList.length; j++) {
+      for (let i = 0; i < devs.length; i++) {
+        let dev = devs[i];
+        if (dev.ssid == deviceList[j]['ssid'] && dev.device == deviceList[j]['devID']) {
+          state_refresh(dev, j);
+          break;
+        }
+      }
+    }
   });
 
   evt_src.addEventListener("updated", (ev) => { // Device send updated data
     dev = JSON.parse(ev.data);
-    state_refresh(dev);
+    for (let j = 0; j < deviceList.length; j++) {
+      if (dev.ssid == deviceList[j]['ssid'] && dev.device == deviceList[j]['devID']) {
+        state_refresh(dev, j);
+      }
+    }
   });
 
   evt_src.addEventListener("online", (ev) => { //Device online status changed 
     dev = JSON.parse(ev.data);
-    state_refresh(dev);
+    for (let j = 0; j < deviceList.length; j++) {
+      if (dev.ssid == deviceList[j]['ssid'] && dev.device == deviceList[j]['devID']) {
+        state_refresh(dev, j);
+      }
+    }
   });
-}
 
+
+}
